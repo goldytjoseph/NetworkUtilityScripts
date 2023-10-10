bcaa1f91e64c54a141fee802c0f4f9f1ac25be02
soorajkadamberi <145643860+soorajkadamberi@users.noreply.github.com>
2023-10-10 12:01:38 +0530

    Update networkScanV2.py
    added commend

--- networkScanV2.py
+++ networkScanV2.py
@@ -4,6 +4,7 @@ import struct
 import sys
 import subprocess
 
+# Function to convert a subnet into an IP range and check the status of each IP
 def subnet_to_ip_range(subnet):
     try:
         d, s, c = socket.inet_aton, struct, '!I'
@@ -17,12 +18,14 @@ def subnet_to_ip_range(subnet):
             w = v | ((1 << t) - 1)
             return g(v), g(w), [g(x) for x in range(v, w + 1)]
 
+        # Extract start, end, and list of all IPs from the subnet
         start, end, ips = h(*subnet.split('/'))
 
         print(f'Subnet: {subnet}')
         print(f'IP Range: {start} - {end}')
         print('All IPs:')
 
+        # Check the status (up or down) of each IP address in the range
         for i in ips:
             status = "up" if ping(i) else "down"
             color = "\033[32m" if status == "up" else "\033[31m"
@@ -36,6 +39,7 @@ def subnet_to_ip_range(subnet):
         print("\nScan interrupted by user.")
         sys.exit(1)
 
+# Function to ping an IP address and check if it's up
 def ping(ip):
     try:
         output = subprocess.check_output(['ping', '-c', '1', '-W', '1', ip])
@@ -46,7 +50,8 @@ def ping(ip):
     except subprocess.CalledProcessError:
         return False
 
-if name == "main":
+# Check if the script is being run as the main program
+if __name__ == "main":
     if len(sys.argv) < 2:
         print("Usage: python subnet_to_iprange.py <subnet>")
         print("Example: python subnet_to_iprange.py 192.168.1.0/24")
