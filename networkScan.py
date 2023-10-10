caa912dacbcc8094efae7e3feaf00257f12f3648
Aishwaryaks177 <147473817+Aishwaryaks177@users.noreply.github.com>
2023-10-10 12:02:15 +0530

    Update networkScan.py
    add comments

--- networkScan.py
+++ networkScan.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python3
-import socket,struct
+import socket
+import struct
 import sys
 import subprocess
 
-d,s,c=socket.inet_aton,struct,'!I'
-f=lambda n:s.pack(c,n)
-g=lambda n:socket.inet_ntoa(f(n))
+# Helper functions to convert between IP addresses and integers
+d, s, c = socket.inet_aton, struct, '!I'
+f = lambda n: s.pack(c, n)
+g = lambda n: socket.inet_ntoa(f(n))
 
-def h(n,b):
-    t=32-int(b)
-    u=s.unpack(c,d(n))[0]
-    v=(u>>t)<<t
-    w=v|((1<<t)-1)
-    return g(v),g(w),[g(x) for x in range(v,w+1)]
+# Function to calculate IP range from subnet and return a list of IPs
+def h(n, b):
+    t = 32 - int(b)
+    u = s.unpack(c, d(n))[0]
+    v = (u >> t) << t
+    w = v | ((1 << t) - 1)
+    return g(v), g(w), [g(x) for x in range(v, w + 1)]
 
+# Function to ping an IP address
 def ping(ip):
     try:
         output = subprocess.check_output(['ping', '-c', '1', '-W', '1', ip])
@@ -24,18 +27,22 @@ def ping(ip):
     except subprocess.CalledProcessError:
         return False
 
+# Check if the correct number of command-line arguments is provided
 if len(sys.argv) < 2:
     print("Usage: python subnet_to_iprange.py <subnet>")
     print("Example: python subnet_to_iprange.py 192.168.1.0/24")
     sys.exit(1)
 
+# Parse the subnet argument and calculate the IP range
 subnet = sys.argv[1]
-start,end,ips=h(*subnet.split('/'))
+start, end, ips = h(*subnet.split('/'))
 
+# Print subnet information
 print(f'Subnet: {subnet}')
 print(f'IP Range: {start} - {end}')
 print('All IPs:')
 
+# Iterate through the list of IPs in the subnet and ping each one
 for i in ips:
     status = "up" if ping(i) else "down"
     color = "\033[32m" if status == "up" else "\033[31m"
