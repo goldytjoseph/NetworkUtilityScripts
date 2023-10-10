e1d59a42fa25275285be3a086d6b80f0ede25f4e
Goldy T Joseph <35062235+goldytjoseph@users.noreply.github.com>
2023-10-10 12:18:45 +0530

    Update networkScanV2.py

--- networkScanV2.py
+++ networkScanV2.py
@@ -3,47 +3,21 @@ import socket
 import struct
 import sys
 import subprocess
-import argparse
 
-def subnet_to_ip_range(subnet, port_scan=False, start_port=1, end_port=1024, timeout=1):
-    try:
-        d, s, c = socket.inet_aton, struct, '!I'
-        f = lambda n: s.pack(c, n)
-        g = lambda n: socket.inet_ntoa(f(n))
+# Helper functions to convert between IP addresses and integers
 
-        def h(n, b):
-            t = 32 - int(b)
-            u = s.unpack(c, d(n))[0]
-            v = (u >> t) << t
-            w = v | ((1 << t) - 1)
-            return g(v), g(w), [g(x) for x in range(v, w + 1)]
+d, s, c = socket.inet_aton, struct, '!I'
+f = lambda n: s.pack(c, n)
+g = lambda n: socket.inet_ntoa(f(n))
 
-        start, end, ips = h(*subnet.split('/'))
+def h(n, b):
+    t = 32 - int(b)
+    u = s.unpack(c, d(n))[0]
+    v = (u >> t) << t
+    w = v | ((1 << t) - 1)
+    return g(v), g(w), [g(x) for x in range(v, w + 1)]
 
-        print(f'Subnet: {subnet}')
-        print(f'IP Range: {start} - {end}')
-        print('All IPs:')
-
-        for i in ips:
-            status = "up" if ping(i) else "down"
-            color = "\033[32m" if status == "up" else "\033[31m"
-            print(f'{color}{i}  \033[0m')
-
-        if port_scan:  
-            print('Port Scan Results:')
-            for i in ips:
-                open_ports = scan_ports(i, range(start_port, end_port + 1), timeout)
-                if open_ports:
-                    print(f'{i}: Open Ports - {open_ports}')
-
-    except (ValueError, IndexError, socket.error) as e:
-        print(f"Error: {e}")
-    except subprocess.CalledProcessError:
-        print("Error: Unable to execute the 'ping' command.")
-    except KeyboardInterrupt:
-        print("\nScan interrupted by user.")
-        sys.exit(1)
-
+# Function to ping an IP address
 def ping(ip):
     try:
         output = subprocess.check_output(['ping', '-c', '1', '-W', '1', ip])
@@ -54,24 +28,35 @@ def ping(ip):
     except subprocess.CalledProcessError:
         return False
 
-def scan_ports(ip, ports, timeout):
-    open_ports = []
-    for port in ports:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(timeout)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            open_ports.append(port)
-        sock.close()
-    return open_ports
+<<<<<<< patch-3
+def resolve_hostname(ip):
+    try:
+        hostname, _, _ = socket.gethostbyaddr(ip)
+        return hostname
+    except (socket.herror, socket.gaierror):
+        return "N/A"
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Subnet to IP Range Converter with Port Scan')
-    parser.add_argument('subnet', type=str, help='The subnet to convert to an IP range (e.g., 192.168.1.0/24)')
-    parser.add_argument('-p', '--port-scan', action='store_true', help='Perform a port scan on each IP address')
-    parser.add_argument('--start-port', type=int, default=1, help='Start port for port scan (default: 1)')
-    parser.add_argument('--end-port', type=int, default=1024, help='End port for port scan (default: 1024)')
-    parser.add_argument('--timeout', type=int, default=1, help='Timeout for port scan in seconds (default: 1)')
+=======
+# Check if the correct number of command-line arguments is provided
+>>>>>>> main
+if len(sys.argv) < 2:
+    print("Usage: python subnet_to_iprange.py <subnet>")
+    print("Example: python subnet_to_iprange.py 192.168.1.0/24")
+    sys.exit(1)
 
-    args = parser.parse_args()
-    subnet_to_ip_range(args.subnet, args.port_scan, args.start_port, args.end_port, args.timeout)
+# Parse the subnet argument and calculate the IP range
+subnet = sys.argv[1]
+start, end, ips = h(*subnet.split('/'))
+
+# Print subnet information
+print(f'Subnet: {subnet}')
+print(f'IP Range: {start} - {end}')
+print('All IPs:')
+
+# Iterate through the list of IPs in the subnet and ping each one
+for i in ips:
+    status = "up" if ping(i) else "down"
+    color = "\033[32m" if status == "up" else "\033[31m"
+    hostname = resolve_hostname(i)
+    print(f'{color}{i} ({hostname})  \033[0m (Status: {status})')
+
